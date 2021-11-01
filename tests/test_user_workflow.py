73d371e87a4d37f428feb4115e53cd4d0b619a84
florian-huber <florian.huber@hs-duesseldorf.de>
2021-11-01 14:54:51 +0100

    correct path in test

--- tests/test_user_workflow.py
+++ tests/test_user_workflow.py
@@ -6,12 +6,12 @@ import memo_ms as memo
 import numpy as np
 import os
 import pandas as pd
-from pathlib import Path
 import plotly.express as px
 import pytest
 
 
-TEST_RESOURCES_PATH = Path(os.path.dirname(Path(__file__).parent)) / 'data'
+PATH_ROOT = os.path.dirname(os.path.dirname((__file__)))
+PATH_TEST_RESOURCES = os.path.join(PATH_ROOT, 'data')
 
 @pytest.mark.integtest
 def test_workflow():
@@ -44,7 +44,7 @@ def test_workflow():
             return 'What is it? :)'
 
 
-    metadata_filename = TEST_RESOURCES_PATH / "1901_gradient_benchmarking_dataset_v4_sample_metadata.txt"
+    metadata_filename = os.path.join(PATH_TEST_RESOURCES, "1901_gradient_benchmarking_dataset_v4_sample_metadata.txt")
     df_meta = pd.read_csv(metadata_filename, sep='\t')
     df_meta['Samplename'] = df_meta['Samplename'].str[:-6]
     df_meta['Samplename'] = df_meta['Samplename'].str.replace('BLANK_', 'BLANK')
@@ -57,14 +57,14 @@ def test_workflow():
     # Import feature_quant table
     # To compute the MEMO matrix of the dataset, we need the table reporting presence/absence of each metabolite in each sample. This information is in the quant table and we create a memo.FeatureTable dataclass object to load it.
 
-    table_filename = TEST_RESOURCES_PATH / "quantification_table-00000.csv"
+    table_filename = os.path.join(PATH_TEST_RESOURCES, "quantification_table-00000.csv")
     feat_table_qe = memo.FeatureTable(path=table_filename)
     feat_table_qe.quant_table
 
     # Import spectra
     # Since MEMO relies on the occurence of MS2 fragments/losses in samples to compare them, we obviously need to importthe features' fragmentation spectra. Losses are computed and spectra translated into documents. Store in memo.SpectraDocuments dataclass object.
 
-    specs_filename = TEST_RESOURCES_PATH / "qemistree_specs_ms.mgf"
+    specs_filename = os.path.join(PATH_TEST_RESOURCES, "qemistree_specs_ms.mgf")
     spectra_qe = memo.SpectraDocuments(path=specs_filename, min_relative_intensity = 0.01,
                 max_relative_intensity = 1, min_peaks_required=5, losses_from = 10, losses_to = 200, n_decimals = 2)
     spectra_qe.document
