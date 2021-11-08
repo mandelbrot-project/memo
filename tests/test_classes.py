5af05b4b9c5932da6d1982aafdea2b3591e3eff2
florian-huber <florian.huber@hs-duesseldorf.de>
2021-11-08 21:22:45 +0100

    add test for featureTable

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import memo_ms as memo
 
 
@@ -23,6 +24,18 @@ def test_spectra_documents():
         "Expected different precursor m/z values"
     assert spectra.document.documents[0][0] == "peak@71.05", \
         "Expected differnt word in document"
+    assert spectra.document.documents[1][-1] == "loss@194.11", \
+        "Expected differnt word in document"
     assert spectra.document.documents[2][-1] == "peak@445.11", \
         "Expected differnt word in document"
-    
+
+
+def test_feature_table():
+    filename = os.path.join(PATH_TEST_RESOURCES, "test_table.csv")
+    table = memo.FeatureTable(filename)
+    assert table.quant_table.shape == (198, 3), "Expected different table shape"
+    assert table.quant_table.index[0] == "QEC18_Blank_resusp_20181227024429.mzML", \
+        "Expected different filename in table index"
+    expected_first_row = np.array([8.73952054e+08, 1.48157905e+08, 9.83701048e+07])
+    np.testing.assert_almost_equal(table.quant_table.to_numpy()[0, :],
+                                   expected_first_row, decimal=0)
