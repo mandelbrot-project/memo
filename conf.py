a2255ed53657d3938dd72470c8ce9da1be433597
PMA <pierre-marie.allard@unifr.ch>
2021-12-16 22:32:41 +0100

    project_copyright should solve lint error see https://github.com/sphinx-doc/sphinx/issues/8132

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "memo"
-copyright = "2021, Arnaud Gaudry"
+project_copyright = "2021, Arnaud Gaudry"
 author = "Arnaud Gaudry"
 
 
