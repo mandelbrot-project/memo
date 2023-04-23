6f40753347135d363105c13dfb2619fe5fd7a2e0
PMA <pierre-marie.allard@unifr.ch>
2023-04-23 07:57:02 +0200

    a set() in order to avoid a wrong double counting of files in the memo_from_unaligned_samples() function.

--- memo_ms/classes.py
+++ memo_ms/classes.py
@@ -205,10 +205,12 @@ class MemoMatrix:
         dic_memo = {}
         mgf_file = []
         path_to_mg_file = []
+        files_counted = set()  # Keep track of files already counted
         
         for (root, _, files) in os.walk(path_to_samples_dir, topdown=True):
             for file in files:                
-                if file.endswith(pattern_to_match):
+                if file.endswith(pattern_to_match) and file not in files_counted:
+                    files_counted.add(file)
                     path_to_match_file = os.path.join(root, file)
                     path_to_mg_file.append(path_to_match_file)
                     mgf_file.append(file)
